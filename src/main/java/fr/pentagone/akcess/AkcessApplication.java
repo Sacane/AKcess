2c88e822eb0906bafef93e9cadda9d3db7c262a9
Ramaroson Rakotomihamina Johan <johan.ramaroson@gmail.com>
2023-11-22 20:22:06 +0100

    init project

---
+++ src/main/java/fr/pentagone/akcess/AkcessApplication.java
@@ -0,0 +1,13 @@
+package fr.pentagone.akcess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkcessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/pentagone/akcess/AkcessApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.pentagone.akcess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
