f44d46b014396d2fb6b2cf2c006aa37b7284a079
Sacane <johan.ramaroson@gmail.com>
2023-12-07 19:39:14 +0100

    Add base class controllers

---
+++ src/main/java/fr/pentagone/akcess/controller/ManagerController.java
@@ -0,0 +1,9 @@
+package fr.pentagone.akcess.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/managers")
+public class ManagerController {
+}
