e5b0ff714485ce1902f292b4ea00405f3cc714a3
Sacane <johan.ramaroson@gmail.com>
2024-01-07 00:39:47 +0100

    Add extendable cors origin filter for http security

--- src/main/java/fr/pentagone/akcess/security/AkcessCorsOriginFilter.java
+++ src/main/java/fr/pentagone/akcess/security/AkcessCorsOriginFilter.java
@@ -4,20 +4,18 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.logging.Logger;
 
-public class CorsOriginFilter extends OncePerRequestFilter {
+@Component
+public class AkcessCorsOriginFilter extends OncePerRequestFilter {
     private static final Logger LOGGER = Logger.getLogger("CorsOriginFilter");
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        if(request.getServletPath().contains("editor")) LOGGER.info("TEST FILTER");
-        LOGGER.info("servlet path : " + request.getServletPath());
-        LOGGER.info("path : " + request.getContextPath());
-        LOGGER.info("URL : " + request.getRequestURL());
-        LOGGER.info("URI : " + request.getRequestURI());
+        LOGGER.info("Cors Origin filter");
         var origin = request.getHeaders("Origin").asIterator().next();
         if(origin.contains("editor.swagger")) {
             response.addHeader("Access-Control-Allow-Origin", origin);
@@ -25,9 +23,9 @@ public class CorsOriginFilter extends OncePerRequestFilter {
             response.setHeader("Access-Control-Allow-Credentials", "true");
             response.setHeader("Access-Control-Allow-Headers", "content-type, x-gwt-module-base, x-gwt-permutation, clientid, longpush");
             LOGGER.info("ACCEPT ORIGIN");
+        } else {
+            LOGGER.severe("This origin is not accepted");
         }
-
-
         filterChain.doFilter(request, response);
     }
 }
--- src/main/java/fr/pentagone/akcess/security/SecurityConfig.java
+++ src/main/java/fr/pentagone/akcess/security/SecurityConfig.java
@@ -1,7 +1,6 @@
 package fr.pentagone.akcess.security;
 
-import org.apache.catalina.filters.CorsFilter;
-import org.apache.tomcat.util.file.ConfigurationSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,30 +13,35 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, BearerTokenFilter bearerTokenFilter) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, BearerTokenFilter bearerTokenFilter, AkcessCorsOriginFilter corsOriginFilter) throws Exception {
         return http.authorizeHttpRequests(custom -> custom.anyRequest().permitAll())
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .cors(configurer -> configurer.configure(http))
                 .csrf(AbstractHttpConfigurer::disable)
-                .headers(customizer -> customizer.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
-                .sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .headers(configurer -> configurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .sessionManagement(configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .httpBasic(AbstractHttpConfigurer::disable)
+                .addFilterBefore(corsOriginFilter, CorsFilter.class)
                 .addFilterBefore(bearerTokenFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
     @Bean
+    public CorsFilter corsFilter() {
+        return new CorsFilter(corsConfigurationSource());
+    }
+
+    @Bean
     public CorsConfigurationSource corsConfigurationSource(){
         var configurationSource = new CorsConfiguration();
-        configurationSource.addAllowedOrigin("https://editor.swagger.io");
         configurationSource.setAllowedMethods(Arrays.asList("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
         var source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configurationSource);
